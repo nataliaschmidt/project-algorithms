12ab4a2dd36eb01d9a682f2c08aebe163fc76ad9
nataliaschmidt <nataliak.schmidt@gmail.com>
2023-09-20 18:56:44 -0300

    Implementa a lógica para o arquivo challeng_palindromes_iterative

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,3 +1,18 @@
 def is_palindrome_iterative(word):
-    """Faça o código aqui."""
+    if not word:
+        return False
+
+    low_index = 0
+    high_index = len(word) - 1
+
+    while low_index <= high_index:
+        if word[low_index] != word[high_index]:
+            return False
+        low_index += 1
+        high_index -= 1
+
+    return True
     raise NotImplementedError
+
+
+print(is_palindrome_iterative("REVIVER"))
