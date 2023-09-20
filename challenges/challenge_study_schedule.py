6f7aee9b2f953552261a18ddb3770f2ea252820a
nataliaschmidt <nataliak.schmidt@gmail.com>
2023-09-20 15:49:07 -0300

    Implementa a lógica para o arquivo challenge_study_schedule.py

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,16 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
+    count_students = 0
+
+    try:
+        for tupla in permanence_period:
+            start_time, end_time = tupla
+            if start_time <= target_time <= end_time:
+                count_students += 1
+    except TypeError:
+        return None
+
+    return count_students
     raise NotImplementedError
+
+
+print(study_schedule([(2, 2), (1, 2), (2, 3), (1, 5), (4, 5), (4, 5)], 2))
