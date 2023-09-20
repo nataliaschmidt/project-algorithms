2beacb06a5b874672524a520220f936a8e992000
nataliaschmidt <nataliak.schmidt@gmail.com>
2023-09-20 20:27:46 -0300

    Implementa os testes para a função encrypt_message

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,29 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+import pytest
+from challenges.challenge_encrypt_message import encrypt_message
 
 
 def test_encrypt_message():
-    pass
+    '''Para uma key par:
+    divide message no índice key,
+    inverte a posição das partes,
+    inverte os caracteres de cada parte,
+    e retorna a união das partes novamente com "_" entre elas'''
+    assert encrypt_message("Let's Go", 4) == "oG s_'teL"
+
+    '''Para uma key ímpar:
+    divide message no índice key,
+    inverte os caracteres de cada parte,
+    e retorna a união das partes novamente com "_" entre elas'''
+    assert encrypt_message("Let's Go", 3) == "teL_oG s'"
+
+    '''Se key não possuir o tipo corretos, uma exceção deve ser lançada'''
+    with pytest.raises(TypeError, match="tipo inválido para key"):
+        encrypt_message("Let's Go", "3")
+
+    '''Se message não possuir o tipo corretos, uma exceção deve ser lançada'''
+    with pytest.raises(TypeError, match="tipo inválido para message"):
+        encrypt_message(3, 3)
+
+    '''Se key não for um índice positivo válido de message,
+    retorna a string message invertida '''
+    assert encrypt_message("Let's Go", 20) == "oG s'teL"
