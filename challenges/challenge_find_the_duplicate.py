6512677b25f5b1e54350b0f0cefa6cc28e3daf89
nataliaschmidt <nataliak.schmidt@gmail.com>
2023-09-20 18:27:39 -0300

    Implementa a lógica para o arquivo challeng_find_the_duplicate

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,3 +1,17 @@
 def find_duplicate(nums):
-    """Faça o código aqui."""
+    if not nums:
+        return False
+
+    seen_numbers = set()
+    for number in nums:
+        if not isinstance(number, (int, float)) or number < 0:
+            return False
+        if number in seen_numbers:
+            return number
+        seen_numbers.add(number)
+        # print(seen_numbers)
+    return False
     raise NotImplementedError
+
+
+# print(find_duplicate([3, 1, 2, 4, 6, 5, 7, 7, 7, 8]))
