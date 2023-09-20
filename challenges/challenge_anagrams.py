b6f02e449e420b3a8e11e8cad70362a9bf132c5a
nataliaschmidt <nataliak.schmidt@gmail.com>
2023-09-20 17:32:02 -0300

    Implementa a lógica para o arquivo challenge_anagrams.py

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,3 +1,49 @@
+def merge_sort(arr):
+    # caso base
+    if len(arr) <= 1:
+        return arr
+
+    middle = len(arr) // 2
+    left = arr[:middle]
+    right = arr[middle:]
+    print(left, right)
+
+    left = merge_sort(left)
+    right = merge_sort(right)
+    return merge(left, right)
+
+
+def merge(left, right):
+    result = ""
+    left_index, right_index = 0, 0
+
+    while left_index < len(left) and right_index < len(right):
+        if left[left_index] < right[right_index]:
+            result += left[left_index]
+            left_index += 1
+        else:
+            result += right[right_index]
+            right_index += 1
+
+    result += left[left_index:]
+    result += right[right_index:]
+
+    return result
+
+
 def is_anagram(first_string, second_string):
-    """Faça o código aqui."""
+    if len(first_string) == 0 and len(second_string) == 0:
+        return first_string, second_string, False
+
+    sorted_first_string = merge_sort(first_string.lower())
+    sorted_second_string = merge_sort(second_string.lower())
+    is_strings_anagram = sorted_first_string == sorted_second_string
+
+    if len(first_string) != len(second_string):
+        return sorted_first_string, sorted_second_string, is_strings_anagram
+
+    return sorted_first_string, sorted_second_string, is_strings_anagram
     raise NotImplementedError
+
+
+print(is_anagram("", "perda"))
